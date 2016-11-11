55b137396c76791a284c42e3c4e6f9715bc3b00e
Saulo <saulocn@gmail.com>
2016-11-11 21:15:25 -0200

    - Consumindo a API HTTP;

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -12,12 +12,14 @@ var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
 var foto_module_1 = require('./foto/foto.module');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, foto_module_1.FotoModule],
+            imports: [platform_browser_1.BrowserModule, foto_module_1.FotoModule, http_1.HttpModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
